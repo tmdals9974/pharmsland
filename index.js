a9f32aee6760a90f51fd1c7e35661d28f1c20ac1
tmdals9974 <tmdals9974@naver.com>
2020-10-22 16:15:55 +0900

    favicon 모듈 설치 및 기능 추가

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var favicon = require('serve-favicon')
 
-app.listen(3000, () => {
-    console.log("start express server on port 3000!!");
-})
+app.use(express.static(__dirname + '/assets'));
+app.use(favicon(__dirname +  '/assets/images/favicon.ico'));
 
-app.use(express.static(__dirname + '/assets'));  //app.use('/static', express.static(__dirname + '/public')); //가상접두부를 통해 마운트 가능 (/static/main.js)
+app.listen(3000, () => { console.log("http://localhost:3000")});
 
 app.all('/', (req, res) => {
     res.sendFile(path.join(__dirname, "./views/main.html"));
